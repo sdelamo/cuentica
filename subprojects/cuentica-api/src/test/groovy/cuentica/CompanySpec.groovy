c1299cf4edc8f9360b42fdb66d4cc2db2ae29198
sdelamo <sergio.delamo@softamo.com>
2018-11-29 13:08:26 +0100

    extract to shared field

--- subprojects/cuentica-api/src/test/groovy/cuentica/CompanySpec.groovy
+++ subprojects/cuentica-api/src/test/groovy/cuentica/CompanySpec.groovy
@@ -18,15 +18,16 @@ package cuentica
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.HttpStatus
 import spock.lang.Requires
+import spock.lang.Shared
 import spock.lang.Specification
 
 class CompanySpec extends Specification {
 
+    @Shared
+    Cuentica cuentica = new Cuentica(System.getProperty('cuentica.token'))
+
     @Requires({ sys['cuentica.token']})
     void "a company can be fetched"() {
-        given:
-        Cuentica cuentica = new Cuentica(System.getProperty('cuentica.token'))
-
         when:
         HttpResponse<Company> response = cuentica.fetchCompany().blockingFirst()
 
